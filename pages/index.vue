613634003fb876a93f29f9d350309365e8ce718e
takuma-ru <kcattdev@gmail.com>
2021-09-16 22:50:01 +0900

    [Add] setup.

---
+++ pages/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    {{ id }}
+  </div>
+</template>
+
+<script lang="ts">
+import { computed, defineComponent } from '@nuxtjs/composition-api'
+
+export default defineComponent({
+  setup (_, { root }) {
+    const route = root.$route
+    const id = computed(() => route.params.id)
+
+    console.log(id)
+
+    return {
+      route,
+      id
+    }
+  }
+})
+</script>
---
+++ pages/inspire.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    {{ id }}
+  </div>
+</template>
+
+<script lang="ts">
+import { computed, defineComponent, useRoute } from '@nuxtjs/composition-api'
+
+export default defineComponent({
+  setup () {
+    const route = useRoute()
+    const id = computed(() => route.value.query.id) // ?id=asd
+
+    return {
+      route,
+      id
+    }
+  }
+})
+</script>
